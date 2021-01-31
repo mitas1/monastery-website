2922191f873c5f6038baed749792e14b880cbf01
Samuel Mitas <samomitas@gmail.com>
2021-01-31 20:39:08 +0100

    preview: Add required fields

--- pages/api/preview.ts
+++ pages/api/preview.ts
@@ -10,18 +10,19 @@ export default async (req, res) => {
         *[_type == "post" && slug.current == "${req.query.slug}"]{
             _id,
             title,
+            publishAt,
             body,
             'slug': slug.current,
             'category': categories[0]->slug.current,
         }[0]
     `);
 
-    if (!post) {
+    if (!post || !post.slug) {
         return res.status(401).json({ message: "Invalid slug" });
     }
 
-    if (!(post.category && post.title && post.body)) {
-        return res.status(401).json({ message: "Required fields missing" });
+    if (!(post.category && post.title && post.body && post.publishAt)) {
+        return res.status(401).json({ message: "Required fields are missing" });
     }
 
     res.setPreviewData({});

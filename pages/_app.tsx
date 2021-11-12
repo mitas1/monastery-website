4bf0ac0030c36ceb318e6ecc1ef657983326322b
Samuel Mitas <samomitas@gmail.com>
2021-11-12 23:36:37 +0100

    Setup tailwind

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,15 @@
 import '@material/react-drawer/dist/drawer.css';
 import 'react-medium-image-zoom/dist/styles.css';
+import 'tailwindcss/tailwind.css';
 
 import Router from 'next/router';
 import NProgress from 'nprogress';
 
-Router.events.on('routeChangeStart', () => {
+Router.events.on("routeChangeStart", () => {
     NProgress.start();
 });
-Router.events.on('routeChangeComplete', () => NProgress.done());
-Router.events.on('routeChangeError', () => NProgress.done());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
     return <Component {...pageProps} />;

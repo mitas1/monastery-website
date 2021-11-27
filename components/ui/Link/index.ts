4776a3f2a9572f61c52efa539c980c94fdc88d25
Samuel Mitas <mitas1@users.noreply.github.com>
2021-11-27 14:17:43 +0100

    Update footer (#28)
    * Update locales
    
    * Add .editorconfig
    
    * Add prettier config
    
    * Update footer
    
    Co-authored-by: durinca <38999042+durinca@users.noreply.github.com>

--- components/ui/Link/index.ts
+++ components/ui/Link/index.ts
@@ -1,2 +1,3 @@
-export { default as Link } from './Link';
 export { default as ArrowLink } from './ArrowLink';
+export { default as ExternalLink } from './ExternalLink';
+export { default as Link } from './Link';

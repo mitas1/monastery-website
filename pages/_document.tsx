65b0eece23dcebb38a386e7f83acebc78ed8da5b
Samuel Mitas <samomitas@gmail.com>
2021-12-02 20:33:02 +0100

    Add favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,8 +1,8 @@
 import Document, {
-  Head,
-  Html,
-  Main,
-  NextScript,
+    Head,
+    Html,
+    Main,
+    NextScript,
 } from 'next/document';
 
 class MyDocument extends Document {
@@ -14,6 +14,13 @@ class MyDocument extends Document {
                         href="https://fonts.googleapis.com/css2?family=Martel:wght@600&family=Roboto:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=optional"
                         rel="stylesheet"
                     />
+                    <link rel="icon" href="/favicon-48.png" type="image/png" />
+                    <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+                    <link
+                        rel="apple-touch-icon"
+                        href="/favicon-apple-touch.png"
+                    />
+                    <link rel="manifest" href="/manifest.json" />
                 </Head>
                 <body>
                     <Main />
